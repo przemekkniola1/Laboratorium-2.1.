a0998d6be538006f25368a4231495e08848412dd
przemekkniola1 <48278909+przemekkniola1@users.noreply.github.com>
2019-03-20 09:22:32 +0100

    Update Potęgowanie.java

--- Potęgowanie.java
+++ Potęgowanie.java
@@ -1,6 +1,22 @@
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+import java.util.*;
+import java.lang.*;
+ 
+class Potegowanie
+{
+public static void main (String[] args) throws java.lang.Exception
+{
+Scanner scanner = new Scanner(System.in);
+int a, b, wynik = 1;
+ 
+a = scanner.nextInt();
+b = scanner.nextInt();
+ 
+while(b > 0)
+{
+wynik *= a;
+b--;
+}
+ 
+System.out.println(wynik);
+}
 }
