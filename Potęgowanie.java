4db8eb7b62209a042460cadbf60e66f8e4a24ac6
przemekkniola1 <48278909+przemekkniola1@users.noreply.github.com>
2019-03-20 09:25:34 +0100

    Update Potęgowanie.java

--- Potęgowanie.java
+++ Potęgowanie.java
@@ -1,22 +1,24 @@
+package com.kniolacompany;
+
 import java.util.*;
 import java.lang.*;
- 
-class Potegowanie
+
+class Main
 {
-public static void main (String[] args) throws java.lang.Exception
-{
-Scanner scanner = new Scanner(System.in);
-int a, b, wynik = 1;
- 
-a = scanner.nextInt();
-b = scanner.nextInt();
- 
-while(b > 0)
-{
-wynik *= a;
-b--;
-}
- 
-System.out.println(wynik);
-}
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner scanner = new Scanner(System.in);
+        int a, b, wynik = 1;
+
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+
+        while(b > 0)
+        {
+            wynik *= a;
+            b--;
+        }
+
+        System.out.println(wynik);
+    }
 }
